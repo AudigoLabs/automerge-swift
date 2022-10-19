c945b12ef3f7411e31fcbe33c853396528ec7f54
Brian Gomberg <brian@audigolabs.com>
2022-10-18 18:41:01 -0700

    Make request properties public

--- Source/DataTypes/Request.swift
+++ Source/DataTypes/Request.swift
@@ -1,5 +1,5 @@
 //
-//  File 2.swift
+//  Request.swift
 //  
 //
 //  Created by Lukas Schmidt on 07.04.20.
@@ -9,12 +9,12 @@ import Foundation
 
 public struct Request: Equatable, Codable {
 
-    let startOp: Int
-    let deps: [ObjectId]
-    let message: String
-    let time: Date
-    let actor: Actor
-    let seq: Int
-    let ops: [Op]
+    public let startOp: Int
+    public let deps: [ObjectId]
+    public let message: String
+    public let time: Date
+    public let actor: Actor
+    public let seq: Int
+    public let ops: [Op]
 
 }
